746272b01dba0c1fe1e0e435c4a0ec5798745dc2
Arpan Chatterjee <arpanchatterjeeg@gmail.com>
2022-04-29 12:55:49 +0530

    Added code to check if number is prime

---
+++ Maths/isprime.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+#include<string>
+using namespace std;
+
+bool check_prime_naive(int a){
+    //idea is that square root always come in a pair. the smaller number of this pair has to be withinroot of n.
+    for(int i=2;i<=sqrt(a);i++){
+        if(a%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int a;
+    cin>>a;
+    bool isPrime = check_prime_naive(a);
+    string text;
+    isPrime? text = "": text = "not";
+    cout<<a<<" is "<<text<<" a prime."<<endl;
+    return 0;
+}
