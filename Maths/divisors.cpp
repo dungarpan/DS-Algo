116db5bbd03c87c3e7a7e27ce41adaf758bd2001
Arpan Chatterjee <arpanchatterjeeg@gmail.com>
2022-04-29 16:38:57 +0530

    Code correction for divisors and debugging

--- Maths/divisors.cpp
+++ Maths/divisors.cpp
@@ -8,19 +8,17 @@ void print_divisors(int a){
     //idea is that square root always come in a pair. the smaller number of this pair has to be withinroot of a.
     int i;
     for(int i=1;i<=sqrt(a);i++){
+        cout<<"hello:"<<i<<endl;
         if(a%i==0){
             cout<<i<<" ";
         }
     }
-    if(i*i==a)
-        i--;
-    for(;i>=1;i--){
+    for(i = int(sqrt(a))-1;i>=1;i--){
+        cout<<"i:"<<i<<endl;
         if(a%i==0){
+            //cout<<"i"<<endl;
             cout<<a/i<<" ";
         }
-    }
-    if(a>1){
-        cout<<a;
     }
 }
 
