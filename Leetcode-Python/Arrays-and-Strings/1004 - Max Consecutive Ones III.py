388deb19fc51c355df6796c37386a4dbef8099cd
Arpan Chatterjee <arpanchatterjeeg@gmail.com>
2023-05-20 21:17:19 -0400

    Corrections to 1004

--- Leetcode-Python/Arrays-and-Strings/1004 - Max Consecutive Ones III.py
+++ Leetcode-Python/Arrays-and-Strings/1004 - Max Consecutive Ones III.py
@@ -1,33 +1,12 @@
 def longestOnes(nums, k):
-    # maintain start and end pointer
-    start = end = 0
-    # maintain max length of 1's and current length of 1s
-    max_l = curr_l = 0
-    kc = k
-    while end<len(nums):
-        curr_l=0
-        zc = 0
-        kc=k
-        # set start pointer to next index of start 0 if we run out of k
-        if end!=0:
-            if ns+1<len(nums):
-                start = ns+1
-                end = ns+1
-        while end<len(nums) and (nums[end]==1 or kc>=0):
-            if nums[end]==0:
-                if kc == k:
-                    ns = end
-                if kc==0:
-                    break
-                else:
-                    kc = kc - 1
-                    curr_l = curr_l + 1
-            else:
-                curr_l = curr_l + 1
-            end = end + 1
+    l=0
+    for r,val in enumerate(nums):
+        k -= (1-val)
+        if k<0:
+            k += (1-nums[l])
+            l+=1
 
-        max_l = max(curr_l, max_l)
-    return max_l
+    return r-l+1
     """
     :type nums: List[int]
     :type k: int
@@ -36,8 +15,10 @@ def longestOnes(nums, k):
 
 def main():
     nums = [1,1,1,0,0,0,1,1,1,1,0]
+    nums_fail = [0,0,1,1,1,0,0]
     k = 2
-    ans = longestOnes(nums,k)
+    k_fail=0
+    ans = longestOnes(nums_fail,k_fail)
     print(ans)
 
 main()
